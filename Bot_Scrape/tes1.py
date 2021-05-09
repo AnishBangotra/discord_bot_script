5c93286680e23408a1f87b3a0c26d4d5a9538c19
Anish Bangotra <73160320+AnishBangotra@users.noreply.github.com>
2021-05-09 13:11:33 +0530

    scrape file

--- Bot_Scrape/tes1.py
+++ Bot_Scrape/tes1.py
@@ -9,8 +9,6 @@ from datetime import datetime
 
 footwearURL = "https://www.innvictus.com/jordan/c/jordan?q=%3Arelevance%3Atype%3A100010000000000000"
 tracemalloc.start()
-#newurl = "https://www.innvictus.com/jordan/c/jordan"+"?"+para
-#strt = request.Request(newurl, data)
 
 def fetchData(url):
     main = 'https://www.innvictus.com'
@@ -30,7 +28,6 @@ def fetchData(url):
         number = soup.find('p', {'class' : "product-titles__model"})
         model = number.text
         model = model[7:]
-        #size_stock = soup.find('a', {'class':'product-size__option'}, onclick="ACC.productDetail.getNewProductSize(this)")
         img = soup.find('img', alt=product)
         img_url = main+str(img.get('src'))
         avail = soup.find_all('a', {'class': "buy-button buy-button--buy-now"})
@@ -42,7 +39,6 @@ def fetchData(url):
         else:
             status = 'Out of Stock'
             quantity = 0
-        #print(product, price, model, status, quantity, size, img_url)
         return(product, price, model, status, quantity, size, img_url, link)
     except:pass
 
@@ -51,7 +47,6 @@ def scrape(url):
         main = 'https://www.innvictus.com'
         r = requests.get(url).text
         soup = BeautifulSoup(r, 'lxml')
-        #div = soup.find_all('span', class_="is-gridwallCard__item__name")
         div1 = soup.find_all('a', class_="js-gtm-product-click")
         links=list()
         data=list()
@@ -64,7 +59,6 @@ def scrape(url):
             final_url = main+i
             product, price, model, status, quantity, size, img_url, link = fetchData(final_url)
             data.append([product, price, model, status, quantity, size, img_url, link])
-        #print(data)
         return data
 
 def fetchPages(url):
@@ -77,7 +71,4 @@ def fetchPages(url):
         url = "https://www.innvictus.com/jordan/c/jordan?q=%3Arelevance%3Atype%3A100010000000000000"
         page+=1
         url = url+'&page='+str(page)
-        #result += data
-        #print(result)
     return result
-#print(fetchPages("https://www.innvictus.com/jordan/c/jordan?q=%3Arelevance%3Atype%3A100010000000000000"))
